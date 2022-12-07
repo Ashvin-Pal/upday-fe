482d4e7fe4dd5c87fbca2a7836d139e6ad724783
Ashvin-Pal <ashvinshash@gmail.com>
2022-12-07 18:07:22 +0100

    feat(components): add loading indicator

---
+++ src/components/loading/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+import "./LoadingIndicator.css";
+
+export function LoadingIndicator() {
+	return (
+		<div className="LoadingIndicator" data-testid="loading-indicator">
+			<div className="LoadingIndicator__Indicator"></div>
+		</div>
+	);
+}
